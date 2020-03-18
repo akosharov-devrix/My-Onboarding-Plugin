1a28fd2951c0fc220dfb476e8a5528d97a5455f0
Atanas Kosharov <akosharov@devrix.com>
2020-03-18 20:50:27 +0200

    Feature: Day 5 content filters

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -14,4 +14,48 @@
  * @package my-onboarding-plugin
  */
 
-// This a test. Test branch and pull request.
+/**
+ * Content prepend filter function
+ *
+ * @param string $content content value.
+ */
+function mop_prepend_filter_the_content( $content ) {
+	$content = 'Onboarding Filter: ' . $content;
+	return $content;
+}
+
+/**
+ * Content append filter function
+ *
+ * @param string $content content value.
+ */
+function mop_append_filter_the_content( $content ) {
+	$content .= 'By Atanas Kosharov';
+	return $content;
+}
+
+/**
+ * Content replace filter function
+ *
+ * @param string $content content value.
+ */
+function mop_replace_filter_the_content( $content ) {
+	$content = preg_replace( '/(<\/p>)/i', '${1}<div style="display: none;"></div>', $content, 1 );
+	return $content;
+}
+
+/**
+ * Content paragraph filter function
+ *
+ * @param string $content content value.
+ */
+function mop_paragraph_filter_the_content( $content ) {
+	$content = '<p>Some paragraph</p>' . $content;
+	return $content;
+}
+
+// Set filters to the_conent.
+add_filter( 'the_content', 'mop_prepend_filter_the_content' );
+add_filter( 'the_content', 'mop_append_filter_the_content' );
+add_filter( 'the_content', 'mop_replace_filter_the_content' );
+add_filter( 'the_content', 'mop_paragraph_filter_the_content', 9 );
